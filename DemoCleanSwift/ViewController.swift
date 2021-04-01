119f7caf4d9381df56d16090d309b9fe6f7dea37
Passakorn Siangsanan <passakorn.siangsanan@accenture.com>
2021-04-01 15:48:23 +0700

    Initial Commit

---
+++ DemoCleanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoCleanSwift
+//
+//  Created by passakorn.siangsanan on 1/4/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
